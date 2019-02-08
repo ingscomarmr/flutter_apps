95f2e1991f3ee6438a5c6c2961e1b5ab6adc8fcb
= <=>
2019-02-07 23:09:53 -0600

    agregando funcionalidad para submit y lib rxdart

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,19 +1,34 @@
 import 'dart:async';
 import 'login_validator.dart';
+import 'package:rxdart/rxdart.dart';
+
 class LoginBloc with  LoginValidator{ //vamos a implementar un mixin con validator
-  final _emailController = StreamController<String>.broadcast(); //con el "_" se identifica que una propiedad es privada
+  //vamos a cambiar esta linia final _emailController = StreamController<String>.broadcast(); //con el "_" se identifica que una propiedad es privada
+  final _email = BehaviorSubject<String>();
   final _passwordController = StreamController<String>.broadcast();
+  final _password = BehaviorSubject<String>(); //es es lo mismo que un treamController<String>.broadcast(); pero de rxdart
 
   //sets para controllers para agregar datos al stream
-  Function(String) get changeEmail => _emailController.sink.add;
-  Function(String) get changePassword => _passwordController.sink.add;
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
 
   //geter para controllers para recuperar datos
-  Stream<String> get email => _emailController.stream.transform(emailValidator); //conectamos los transform de las clase de validadores
-  Stream<String> get password => _passwordController.stream.transform(passwordValidator);
+  Stream<String> get email => _email.stream.transform(emailValidator); //conectamos los transform de las clase de validadores
+  Stream<String> get password => _password.stream.transform(passwordValidator);
+
+  //combinamos los Stream para valida rrxdart.dart
+  Stream<bool>   get submitValid => Observable.combineLatest2(email, password, (e,p) => true); 
+
+  //Este metodo esta predefinico y es para enviar los datos
+  submit(){
+    final myEmail = _email.value; //contiene el ultimo valor correcto
+    final myPass = _password.value;
+    
+    print("Login con los datos usuario: $myEmail y password:$myPass");
+  }
 
   void dispose(){ //se debe implemetar forzosamente
-    _emailController.close();
-    _passwordController.close();
+    _email.close();
+    _password.close();
   }
 }
--- lib/src/screen/login_screen.dart
+++ lib/src/screen/login_screen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import '../bloc/login_bloc.dart';
 
-class LoginScreen extends StatelessWidget{
+class LoginScreen extends StatelessWidget {
   final loginBloc = LoginBloc();
 
   @override
-  Widget build(BuildContext context) {   
+  Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: <Widget>[    
-          emailField(),             
-          passwordField(),  
+        children: <Widget>[
+          emailField(),
+          passwordField(),
           SizedBox(height: 20.0),
           submitButtom()
         ],
@@ -19,50 +19,54 @@ class LoginScreen extends StatelessWidget{
     );
   }
 
-  Widget emailField(){
-    return StreamBuilder( //StreamBuilder permite manipular el patron Bloc por medio de Stream
+  Widget emailField() {
+    return StreamBuilder(
+      //StreamBuilder permite manipular el patron Bloc por medio de Stream
       stream: loginBloc.email,
-      builder: (context, snapshot){
+      builder: (context, snapshot) {
         return TextField(
-          onChanged: loginBloc.changeEmail,
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            hintText: "mi-email@email.com",
-            labelText: "Email address",
-            errorText: snapshot.error
-          )
-        );
+            onChanged: loginBloc.changeEmail,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                hintText: "mi-email@email.com",
+                labelText: "Email address",
+                errorText: snapshot.error));
       },
-    );     
-  }
-
-  Widget passwordField(){
-   return new StreamBuilder(
-     stream: loginBloc.password, //pasamos el transformer
-     builder: (context, snapshot){ //trabajamos con el builder
-       return TextField(
-          onChanged: loginBloc.changePassword, //entra a la validacion
-          obscureText: true, //es para que no se vea la clave
-          decoration: InputDecoration(
-          hintText: "Input your password",
-          labelText: "Password",
-          errorText: snapshot.error //si existe un error en el Stream
-        ),
-      );
-     },
-   );
-   
-   
-  }
-
-  Widget submitButtom(){
-    
-    return RaisedButton(
-      child: Text("Login",
-        style: TextStyle(color: Colors.white),
-      ),
-      onPressed: null,
     );
   }
 
+  Widget passwordField() {
+    return new StreamBuilder(
+      stream: loginBloc.password, //pasamos el transformer
+      builder: (context, snapshot) {
+        //trabajamos con el builder
+        return TextField(
+          onChanged: loginBloc.changePassword, //entra a la validacion
+          obscureText: true, //es para que no se vea la clave
+          decoration: InputDecoration(
+              hintText: "Input your password",
+              labelText: "Password",
+              errorText: snapshot.error //si existe un error en el Stream
+              ),
+        );
+      },
+    );
+  }
+
+  Widget submitButtom() {
+    return StreamBuilder(
+      stream: loginBloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          shape: StadiumBorder(),
+          color: Colors.lightBlue,
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: snapshot.hasData ? loginBloc.submit : null,
+        );
+      },
+    );
+  }
 }
